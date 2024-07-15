213ab7b2c70469bbf02dbb552f3e69cf0e3c32e4
successlead2000 <159511156+successlead2000@users.noreply.github.com>
2024-07-15 19:47:14 +0530

    Create HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloWorld {
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld.getGreeting());
+    }
+}
